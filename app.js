a48ec3ae0a9b5711a3e7427e88ed03b385e5b2b3
Javad Sharifzadeh-Najafi <59621765+Jsharifz@users.noreply.github.com>
2020-03-13 18:57:19 -0600

    final edits

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Initial module requirement setup
+const pageInfo = require('./pageInfo');
 const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
@@ -32,13 +33,10 @@ db.once('open', function () {
   console.log('Connected to DB...');
 });
 
-// ///
-
-console.log(db)
-
-
+// This will import the models so the name pair values match with the database
 const ImGallery = require("./models/Galleries")
 
+// To render the gallery page
 app.get('/gallery', function (req, res) {
   ImGallery.find(function (err, result) {
     res.render('gallery', {
@@ -47,10 +45,12 @@ app.get('/gallery', function (req, res) {
   })
 })
 
+// To render the gallery endpoint page
 app.get('/gallery/:id', function (req, res) {
   ImGallery.find(function (err, result) {
     result.forEach(image => {
-      if (`${req.params.id}` === image.id)
+      let PId = Number(req.params.id)
+      if (PId === image.id)
         res.render('image', {
           image
         })
@@ -58,74 +58,10 @@ app.get('/gallery/:id', function (req, res) {
   })
 })
 
-
-// Import of our custom modules
-const pageInfo = require('./pageInfo');
-const gallery = require("./gallery");
-app.locals.gallery = gallery; // to make gallery accessible for views
-
-// app.get('/gallery/:id', function (req, res) { // start of gallery endpoint script
-
-//   // higher order funcion to catch/filter a specific object
-//   const findPhoto = function (myGallery, photoId) { // main function takes 2 vales, the module, and the id
-//     const index = myGallery.findIndex(function (gallery, index) {
-//       return gallery.id === photoId
-//     })
-//     return myGallery[index]
-//   }
-
-//   let PId = Number(req.params.id) // by default req.params.id returns a STRING, Number() ensures the string is converted to an int, otherwise findPhoto(gallery, PId) will return undefined as it expects a number vs a string
-
-//   let photoById = findPhoto(gallery, PId) // this variable accepts an object as a value when invoking the function findPhoto() with givin id of th photo
-
-//   // this function will generate an html page with given :id, it has the same structure as the other pages which allows us to therefore style the page. since photoById is an object with the specific id, dot notation will be used to fetch the specific name value pair of the object.
-
-//   // *********** REFER to gallery.ejs. The endpoint is linked from /gallery/ as the endpoint page is generated using the id of th image, only difference is the src="" links to the full size large image. The endpoint image page simply refers to the id of the image and therefore it is dynamicly generated.
-//   res.send(`
-
-//   <!DOCTYPE html>
-//   <html lang="en">
-
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>${photoById.title}</title> 
-//     <link rel="stylesheet" type="text/css" href="/css/main.css" media="screen" />
-//   </head>
-
-//   <body>
-//     <nav>
-//       <a href="/">Home</a>
-//       <a href="/gallery">Gallery</a>
-//     </nav>
-
-//     <main>
-//   <h1>${photoById.title}</h1>
-
-//     <figure id="endpoint">
-//       <img src="/images/lrg/${photoById.fileName}" alt="${photoById.title}" id="${PId}">
-//       <figcaption>${photoById.title}, by: <em><a href="${photoById.attribution.url}" target="_blank">${photoById.attribution.credit}</a></em> (${photoById.attribution.source})</figcaption>
-//     </figure>
-
-//     </main>
-//     <footer>Javad Sharifzadeh-Najafi - Web Dev 2020 - SAIT</footer>
-
-// </body>
-
-// </html>
-
-//   `);
-// }); // end of gallery endpoint script
-
 // this function renders the home page
 app.get('/', function (req, res) {
   res.render('index', pageInfo.index);
 });
-
-// this function renders the gallery page. It is only using pageInfo to generate the document tile.
-// app.get('/gallery', function (req, res) {
-//   res.render('gallery', pageInfo.gallery);
-// });
 
 // when referencing url of images and stylesheets, public will be the root directory of the page.
 app.use(express.static(path.join(__dirname, "public")))
